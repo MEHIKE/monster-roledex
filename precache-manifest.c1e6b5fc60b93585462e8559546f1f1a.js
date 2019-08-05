04b854ec7496a98af41ff82810c628d34b04755f
mehike <siin.mina@mail.ee>
2019-08-05 18:22:11 +0300

    Updates

---
+++ precache-manifest.c1e6b5fc60b93585462e8559546f1f1a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "115c50e531d5bc62645dde80e670fc46",
+    "url": "/monster-roledex/index.html"
+  },
+  {
+    "revision": "06dec30b417c30ebad66",
+    "url": "/monster-roledex/static/css/main.f4183b1f.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/monster-roledex/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "06dec30b417c30ebad66",
+    "url": "/monster-roledex/static/js/main.80377421.chunk.js"
+  },
+  {
+    "revision": "9ead4abcd4273b0611d6",
+    "url": "/monster-roledex/static/js/runtime~main.9dc36cb8.js"
+  }
+]);
